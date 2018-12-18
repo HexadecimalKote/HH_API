b9f8182b9ebee89c33aff22fc831b2a3323ef0ff
HexadecimalKote <mydeepnetwork@gmail.com>
2018-12-18 04:29:09 +0200

    Add struct Vacancy

---
+++ HeadHunterLibrary/Vacancy.cs
@@ -0,0 +1,31 @@
+﻿namespace HeadHunterLibrary
+{
+    struct Vacancy
+    {
+        string id,
+               name,
+               salary_from,
+               salary_to,
+               employer_name,
+               address,
+               info;
+
+        public Vacancy(dynamic obj)
+        {
+            id = obj.id;
+            name = obj.name;
+
+            salary_from = obj?.salary?.from ?? " ";
+            salary_to = obj?.salary?.to ?? " ";
+
+            employer_name = obj.employer.name;
+
+            if (obj.address != null)
+                address = obj.address.city + " " + obj.address.street + " " + obj.address.building;
+            else
+                address = " ";
+
+            info = obj?.description ?? " ";
+        }
+    }
+}
